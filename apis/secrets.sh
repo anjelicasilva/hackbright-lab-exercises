021807e832a05a74607bcd0ca34ac2d29096a753
anjelicasilva <46766633+anjelicasilva@users.noreply.github.com>
2019-11-22 15:42:27 -0800

    Delete secrets.sh

--- apis/secrets.sh
+++
@@ -1,7 +0,0 @@
-# Secrets for your Eventbrite app
-# Find these at: https://www.eventbrite.ca/account-settings/apps
-# This file should never go into github!
-
-export EVENTBRITE_KEY="L5M5FTTHZC6XY2ZHC2"
-export EVENTBRITE_SECRET="YWPVNZ5NXFVZJPG7BZBOPLMQ56JOITBRZREP2QLNKH3HIECU4L"
-export EVENTBRITE_TOKEN="2B2ZMLDBD6T5I7U4YRLE"
